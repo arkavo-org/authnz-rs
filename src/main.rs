805757a78f6e46a1adf95e56b60a30a2cdadf091
Paul Flynn <paul@arkavo.com>
2024-07-29 13:44:20 -0400

    TLS AASA (#2)
    * Update application configuration and add server settings
    
    The application was updated to include a configuration loader and multiple server settings were added. The changes include the adjustment of authnz-rs v0.1.0 to v0.2.0 in Cargo.toml, environment variable exports in README.md, and usage of TLS and port specifications in src/main.rs. Additional library dependencies have also been included to support the changes.
    
    * Enable secure session based on TLS settings
    
    This commit updates the SessionManagerLayer to use 'settings.tls_enabled' instead of hardcoding a 'false' value. This allows the secure session setting to adapt based on whether TLS is enabled or not.
    
    * Update domain and relying party details
    
    The domain name and URL have been updated from 'localhost' to 'webauthn.arkavo.net'. The relying party name has also been changed from 'Axum Webauthn-rs' to 'Arkavo'. This ensures the application now points to the correct web domain and properly identifies itself.
    
    * Add env_logger and fix comments and server configuration
    
    Added "env_logger" package to Cargo.toml to enable environment-based logging. Corrected a grammatical error in a comment within src/authn.rs. Made improvements to the server configuration in src/main.rs to properly serve the app and handle potential errors.
    
    * Update routes and dependencies
    
    Renamed registration and authentication routes for consistency and clarity. Also, updated several dependencies in Cargo.toml, including `tokio` to version 1.39.2 and added a minimum rust-version requirement.
    
    * Revise Rust workflow to install Rust before testing
    
    Previously, the workflow assumed Rust was pre-installed on the runner, possibly causing issues if it wasn't. This update ensures Rust is installed using rustup before running tests, leading to a more consistent and reliable CI environment.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
+use std::env;
+use std::path::PathBuf;
 use std::sync::Arc;
 
 use axum::{Extension, Router};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-use axum::routing::post;
-use tokio::sync::Mutex;
+use axum::routing::{get, post};
+use axum_server::tls_rustls::RustlsConfig;
+use tokio::sync::{Mutex, RwLock};
 use tower::ServiceBuilder;
 use tower_sessions::{Expiry, MemoryStore, SessionManagerLayer};
 use tower_sessions::cookie::SameSite;
@@ -18,6 +21,22 @@ mod authn;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+    // Load configuration
+    let settings = load_config().unwrap();
+    // Load and cache the apple-app-site-association.json file
+    let apple_app_site_association = load_apple_app_site_association().await;
+    // Set up TLS if not disabled
+    let tls_config = if settings.tls_enabled {
+        Some(
+            RustlsConfig::from_pem_file(
+                PathBuf::from(settings.tls_cert_path),
+                PathBuf::from(settings.tls_key_path),
+            ).await.unwrap()
+        )
+    } else {
+        None
+    };
     // Create the app
     let app_state = AppState::new();
     let session_store = MemoryStore::default();
@@ -26,20 +45,33 @@ async fn main() {
             SessionManagerLayer::new(session_store)
                 .with_name("authnz-rs")
                 .with_same_site(SameSite::Strict)
-                .with_secure(false) // TODO: change this to true when running on an HTTPS/production server instead of locally
+                .with_secure(settings.tls_enabled)
                 .with_expiry(Expiry::OnInactivity(Duration::seconds(600))),
         );
     // build our application with a route
     let app = Router::<()>::new()
-        .route("/register_start/:username", post(start_register))
-        .route("/register_finish", post(finish_register))
-        .route("/login_start/:username", post(start_authentication))
-        .route("/login_finish", post(finish_authentication))
+        .route("/.well-known/apple-app-site-association", get(serve_apple_app_site_association))
+        .route("/register/:username", get(start_register))
+        .route("/register", post(finish_register))
+        .route("/authenticate/:username", get(start_authentication))
+        .route("/authenticate", post(finish_authentication))
         .layer(Extension(app_state))
         .layer(session_service)
+        .layer(Extension(apple_app_site_association))
         .fallback(handler_404);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = std::net::TcpListener::bind(format!("0.0.0.0:{}", settings.port)).unwrap();
+    println!("Listening on: 0.0.0.0:{}", settings.port);
+    if let Some(tls_config) = tls_config {
+        axum_server::from_tcp_rustls(listener, tls_config)
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    } else {
+        axum_server::from_tcp(listener)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    }
 }
 
 #[derive(Clone)]
@@ -55,25 +87,19 @@ pub struct AppState {
 impl AppState {
     pub fn new() -> Self {
         // Effective domain name.
-        let rp_id = "localhost";
+        let rp_id = "webauthn.arkavo.net";
         // Url containing the effective domain name
         // MUST include the port number!
-        let rp_origin = Url::parse("http://localhost:8080").expect("Invalid URL");
+        let rp_origin = Url::parse("https://webauthn.arkavo.net").expect("Invalid URL");
         let builder = WebauthnBuilder::new(rp_id, &rp_origin).expect("Invalid configuration");
-
-        // Now, with the builder you can define other options.
-        // Set a "nice" relying party name. Has no security properties and
-        // may be changed in the future.
-        let builder = builder.rp_name("Axum Webauthn-rs");
-
+        // Relying party name
+        let builder = builder.rp_name("Arkavo");
         // Consume the builder and create our webauthn instance.
         let webauthn = Arc::new(builder.build().expect("Invalid configuration"));
-
         let users = Arc::new(Mutex::new(AccountData {
             name_to_id: HashMap::new(),
             keys: HashMap::new(),
         }));
-
         AppState { webauthn, accounts: users }
     }
 }
@@ -85,4 +111,58 @@ pub struct AccountData {
 
 async fn handler_404() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, StatusCode::NOT_FOUND.canonical_reason().unwrap())
+}
+
+#[derive(Debug, Clone)]
+struct ServerSettings {
+    port: u16,
+    tls_enabled: bool,
+    tls_cert_path: String,
+    tls_key_path: String,
+    _enable_timing_logs: bool,
+}
+
+fn load_config() -> Result<ServerSettings, Box<dyn std::error::Error>> {
+    let current_dir = env::current_dir()?;
+
+    Ok(ServerSettings {
+        port: env::var("PORT")
+            .unwrap_or_else(|_| "8080".to_string())
+            .parse()?,
+        tls_enabled: env::var("TLS_CERT_PATH").is_ok(),
+        tls_cert_path: env::var("TLS_CERT_PATH").unwrap_or_else(|_| {
+            current_dir
+                .join("fullchain.pem")
+                .to_str()
+                .unwrap()
+                .to_string()
+        }),
+        tls_key_path: env::var("TLS_KEY_PATH").unwrap_or_else(|_| {
+            current_dir
+                .join("privkey.pem")
+                .to_str()
+                .unwrap()
+                .to_string()
+        }),
+        _enable_timing_logs: env::var("ENABLE_TIMING_LOGS")
+            .unwrap_or_else(|_| "false".to_string())
+            .parse()
+            .unwrap_or(false),
+    })
+}
+
+async fn load_apple_app_site_association() -> Arc<RwLock<serde_json::Value>> {
+    let content = tokio::fs::read_to_string("apple-app-site-association.json")
+        .await
+        .expect("Failed to read apple-app-site-association.json");
+    let json: serde_json::Value = serde_json::from_str(&content)
+        .expect("Failed to parse apple-app-site-association.json");
+    Arc::new(RwLock::new(json))
+}
+
+async fn serve_apple_app_site_association(
+    Extension(apple_app_site_association): Extension<Arc<RwLock<serde_json::Value>>>,
+) -> impl IntoResponse {
+    let json = apple_app_site_association.read().await;
+    axum::Json(json.clone())
 }
